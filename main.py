8ac637cebff8a316fe8d5e75c0983958685e2f90
dsdataanalytics2-debug <dsdataanalytics2@gmail.com>
2025-10-21 09:39:49 +0600

    Added delete feature for sell and add history

--- main.py
+++ main.py
@@ -557,5 +557,5 @@ def health_check():
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.getenv("PORT", 8001))
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
